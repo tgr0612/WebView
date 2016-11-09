3f48bb03beb40b6e02795695c6aab7baf56c1d0e
Administrator <Administrator@114.70.235.166>
2016-11-09 18:13:44 +0900

    웹뷰세팅

--- example/src/com/example/example/MainActivity.java
+++ example/src/com/example/example/MainActivity.java
@@ -1,34 +1,65 @@
 package com.example.example;
+import android.webkit.WebView;
 
+import android.webkit.WebViewClient;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+	private WebView mWebView;    // 웹뷰 선언
+	   @Override 
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+	    public void onCreate(Bundle savedInstanceState) {
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	        super.onCreate(savedInstanceState);
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	        setContentView(R.layout.activity_main);
+
+	        
+
+	        mWebView = (WebView) findViewById(R.id.WebView1);
+
+	        mWebView.getSettings().setJavaScriptEnabled(true);      // 웹뷰에서 자바 스크립트 사용
+
+	        mWebView.loadUrl( "192.168.0.1" );            // 웹뷰에서 불러올 URL 입력
+
+	        mWebView.setWebViewClient(new WishWebViewClient());
+
+	    }
+	   @Override
+
+	    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+	    	if((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()){
+
+	    		mWebView.goBack();
+
+	    		return true;    		
+
+	    	}
+
+	        return super.onKeyDown(keyCode, event);
+
+	    }
+
+
+
+	    private class WishWebViewClient extends WebViewClient{
+
+	    	@Override
+
+	    	public boolean shouldOverrideUrlLoading(WebView view, String url){
+
+	    		view.loadUrl(url);
+
+	    		return true;
+
+	    	}
+
+	    }
+
+
 }
